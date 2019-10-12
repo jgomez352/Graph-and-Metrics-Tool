0c46d3e175cd17ea3bc9b9590a3686378d1192bf
Jose F. Gomez <pepe1509@aim.com>
2019-10-12 18:50:45 -0400

    add dynamic generetad chart

--- js/charts.js
+++ js/charts.js
@@ -1,17 +1,19 @@
 // JavaScript source code
 
-var ctx = document.getElementById('LTIVMT').getContext('2d');
 //let dataNumbers = [100, 190, 30, 350, 20, 30];
 //let dataLabels = ['Good Standing','30 to 89 Days','90 to 119 Days','120 to 179 Days','180 to 364 Days','365 and Over'];
 //let dataHeader = 'LTIs'
 
 //Charts parameters
-let chartsDiv = document.getElementById('charts'),
+setTimeout(chartsMaker, 0);
+
+let chartsDiv,
     chartsIDs = ['LTIVMT'],
     chartsH2s = ['Limited Technical Inspections'],
     chartsType = ['bar'],
     chartsFile = ['csv'],
-    chartsURL = ['LTIs2'];
+    chartsURL = ['LTIs2'],
+    chartsList = [];
 
 window.chartColors = {
     red: 'rgb(255, 0, 54, 0.7)',
@@ -31,74 +33,96 @@ window.chartBorderColors = {
     purple: 'rgb(153, 102, 255)',
     grey: 'rgb(201, 203, 207)'
 };
-let myChart = new Chart(ctx, {
-    type: 'bar', 
-    data: {
-        // labels: dataLabels,
-        datasets: [{
-            //label: dataHeader,
-            //data: dataNumbers,
-            backgroundColor: [
-                window.chartColors.green,
-                window.chartColors.orange,
-                window.chartColors.orange,
-                window.chartColors.orange,
-                window.chartColors.orange,
-                window.chartColors.orange
-            ],
-            borderColor: [
-                window.chartBorderColors.green,
-                window.chartBorderColors.red,
-                window.chartBorderColors.red,
-                window.chartBorderColors.red,
-                window.chartBorderColors.red,
-                window.chartBorderColors.red
-            ],
-            borderWidth: 1
-        }]
-    },
-    plugins: [ChartDataSource],
-    options: {
-        legend: {
-            display: false,
-        },
-        scales: {
-            xAxes: [{
-                stacked: false,
-                beginAtZero: true,
-                ticks: {
-                    autoSkip: false
-                }
-            }],            
-            yAxes: [{
-                ticks: {
-
-                    beginAtZero: true
-                }
+function chartsMaker() {
+    let c = 0;
+    const fragment = document.createDocumentFragment();
+    const div = document.createElement('div');
+    let htmlText = `
+<div class="charts" id="charts" style="display:none">
+    <h2>${chartsH2s[c]}</h2>
+    <canvas id="${chartsIDs[c]}" width="400" height="400"></canvas>
+</div>
+`;
+    div.className = 'ChartsContainer';
+    div.innerHTML = htmlText;
+    fragment.appendChild(div);
+    document.body.appendChild(fragment);
+    makeChart(c);
+    chartsDiv = document.getElementById('charts')
+};
+function makeChart(c) {
+    let ctx = document.getElementById(`${chartsIDs[c]}`).getContext('2d');
+    let LTIVMTChart = new Chart(ctx, {
+        type: `${chartsType[c]}`,
+        data: {
+            // labels: dataLabels,
+            datasets: [{
+                //label: dataHeader,
+                //data: dataNumbers,
+                backgroundColor: [
+                    window.chartColors.green,
+                    window.chartColors.orange,
+                    window.chartColors.orange,
+                    window.chartColors.orange,
+                    window.chartColors.orange,
+                    window.chartColors.orange
+                ],
+                borderColor: [
+                    window.chartBorderColors.green,
+                    window.chartBorderColors.red,
+                    window.chartBorderColors.red,
+                    window.chartBorderColors.red,
+                    window.chartBorderColors.red,
+                    window.chartBorderColors.red
+                ],
+                borderWidth: 1
             }]
         },
-        plugins: {
-            datasource: {
-                type: 'csv',
-                url: 'data/LTIs2.csv',
-                delimiter: ',',
-                rowMapping: 'dataset',
-                datasetLabels: true,
-                indexLabels: true
+        plugins: [ChartDataSource],
+        options: {
+            legend: {
+                display: false,
+            },
+            scales: {
+                xAxes: [{
+                    stacked: false,
+                    beginAtZero: true,
+                    ticks: {
+                        autoSkip: false
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+
+                        beginAtZero: true
+                    }
+                }]
+            },
+            plugins: {
+                datasource: {
+                    type: `${chartsFile[c]}`,
+                    url: `data/${chartsURL[c]}.${chartsFile[c]}`,
+                    delimiter: ',',
+                    rowMapping: 'dataset',
+                    datasetLabels: true,
+                    indexLabels: true
+                }
             }
         }
-    }
-   
-});
+
+    });
+    chartsList[c] = LTIVMTChart;
+};
 function LocationFilter() {
     let lSelect = document.getElementById('Location-select');
     let lIndex = lSelect.selectedIndex;
-    let lValue =lSelect[lIndex].value;
-    let lOption = lSelect[lIndex].text  
+    let lValue = lSelect[lIndex].value;
+    let lOption = lSelect[lIndex].text
     //console.log('Value: ' + lValue);
     //console.log('Index: ' + lIndex);
     //console.log('Selected Option: ' + lOption);
-    let ci = myChart;
+    let ci = chartsList[0];
+
     let meta;
     let i = 0;
     if (lValue != 'nothing') {
@@ -138,15 +162,3 @@ function LocationFilter() {
     }
 
 };
-function chartsMaker() {
-    const fragment = document.createDocumentFragment();
-    const div = document.createElement('div');
-    let htmlText = `
-
-`;
-    div.className = 'ChartsContainer';
-    div.innerHTML = htmlText;
-    fragment.appendChild(div);
-    document.body.appendChild(fragment);
-};
-
